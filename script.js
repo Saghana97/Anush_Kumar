dd0b76fe4ddc98acda6e56e44643e82f5df28d8f
anush6898 <anush.kumar@codingmart.com>
2019-12-24 17:17:21 +0530

    center content finish

--- script.js
+++ script.js
@@ -47,3 +47,248 @@ document.getElementById("month-li").addEventListener("click",function(){
 document.getElementById("filter-btn").addEventListener("click",function(){
     document.getElementById("plus-img").classList.add("anim-img-active");
 });
+
+//swipe the graph
+document.getElementById("graph-btn-right").addEventListener("click",function(){
+    document.getElementById("graph-sec").style.transform = "translateX(-550px)";
+    document.getElementById("graph-btn-right").style.display = "none";
+    document.getElementById("graph-btn-left").style.display = "block";
+});
+document.getElementById("graph-btn-left").addEventListener("click",function(){
+    document.getElementById("graph-sec").style.transform = "translateX(0px)";
+    document.getElementById("graph-btn-left").style.display = "none";
+    document.getElementById("graph-btn-right").style.display = "block";
+});
+
+//insert table rows dynamically
+document.getElementById("ins-btn").addEventListener("click",function(){
+    document.getElementById("modal-box").style.display = "block";
+    document.getElementById("main").style.overflow = "hidden";
+});
+
+document.getElementById("add-btn").addEventListener("click",function(){
+    var cell_count = 0;
+    var table = document.getElementById("table");
+    var tot_rows = (table.rows.length)-1;
+    var row = table.insertRow(tot_rows+1);
+    row.insertCell(cell_count).innerHTML = document.getElementById("input1").value;
+    row.insertCell(cell_count+1).innerHTML = document.getElementById("input2").value;
+    row.insertCell(cell_count+2).innerHTML = document.getElementById("input3").value;
+    row.insertCell(cell_count+3).innerHTML = document.getElementById("input4").value;
+    row.insertCell(cell_count+4).innerHTML = document.getElementById("input5").value;
+    row.insertCell(cell_count+5).innerHTML = document.getElementById("input6").value;
+    row.insertCell(cell_count+6).innerHTML = document.getElementById("input7").value;
+
+    document.getElementById("modal-box").style.display = "none";
+    document.getElementById("main").style.overflow = "auto";
+
+    document.getElementById("input1").value="";
+    document.getElementById("input2").value="";
+    document.getElementById("input3").value="";
+    document.getElementById("input4").value="";
+    document.getElementById("input5").value="";
+    document.getElementById("input6").value="";
+    document.getElementById("input7").value="";
+});
+document.getElementById("close-btn").addEventListener("click",function(){
+    document.getElementById("modal-box").style.display = "none";
+    document.getElementById("main").style.overflow = "auto";
+});
+
+
+//graph
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Babby Brown', 'MAC Cosmetics'],
+        datasets: [{
+            label: '',
+            data: [46500, 51000],
+            backgroundColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    fontSize: 9,
+                    fontFamily: 'sans-serif',
+                },
+                barPercentage: 0.4
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+var ctx2 = document.getElementById('myChart2');
+var myChart2 = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['Babby Brown', 'MAC Cosmetics'],
+        datasets: [{
+            label: '',
+            data: [30000, 40000],
+            backgroundColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    fontSize: 9,
+                    fontFamily: 'sans-serif',
+                },
+                barPercentage: 0.4
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+var ctx3 = document.getElementById('myChart3');
+var myChart3 = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: ['Babby Brown', 'MAC Cosmetics'],
+        datasets: [{
+            label: '',
+            data: [800, 3600],
+            backgroundColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    fontSize: 9,
+                    fontFamily: 'sans-serif',
+                },
+                barPercentage: 0.4
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+var ctx4 = document.getElementById('myChart4');
+var myChart4 = new Chart(ctx4, {
+    type: 'bar',
+    data: {
+        labels: ['Babby Brown', 'MAC Cosmetics'],
+        datasets: [{
+            label: '',
+            data: [250, 450],
+            backgroundColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    fontSize: 9,
+                    fontFamily: 'sans-serif',
+                },
+                barPercentage: 0.4
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+var ctx5 = document.getElementById('myChart5');
+var myChart5 = new Chart(ctx5, {
+    type: 'bar',
+    data: {
+        labels: ['Babby Brown', 'MAC Cosmetics'],
+        datasets: [{
+            label: '',
+            data: [800, 3600],
+            backgroundColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderColor: [
+                '#0298db',
+                '#7fdde9'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    fontSize: 9,
+                    fontFamily: 'sans-serif',
+                },
+                barPercentage: 0.4
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
